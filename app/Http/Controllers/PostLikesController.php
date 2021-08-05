4460eb25aa859289c5347526303a658da30b1ef7
salmank0 <mohammadsalmank00@gmail.com>
2021-08-05 15:14:42 +0530

    third commit

--- app/Http/Controllers/PostLikesController.php
+++ app/Http/Controllers/PostLikesController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Mail\PostLiked;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class PostLikesController extends Controller
 {
@@ -24,6 +26,10 @@ class PostLikesController extends Controller
         $post->likes()->create([
             'user_id' => $request->user()->id,
         ]);
+
+        if (!$post->likes()->onlyTrashed()->where('user_id', $request->user()->id)->count()) {
+            Mail::to($post->user)->send(new PostLiked(auth()->user(), $post));
+        }
 
         return back();
     }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,59 +3,30 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            <form action="{{ route('posts') }}" method="post" class="mb-4">
+            @auth
+                <form action="{{ route('posts') }}" method="post" class="mb-4">
 
-                @csrf
-                <div class="mb-4">
-                    <label for="email" class="sr-only">Body</label>
-                    <textarea name="body" id="body" cols="30" rows="4" placeholder="Post something..." class="bg-gray-200 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror"></textarea>
-                    
-                    @error('body')
-                        <div class="mt-2 text-red-500 text-sm">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-                <div>
-                    <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium">Post</button>
-                </div>
+                    @csrf
+                    <div class="mb-4">
+                        <label for="email" class="sr-only">Body</label>
+                        <textarea name="body" id="body" cols="30" rows="4" placeholder="Post something..." class="bg-gray-200 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror"></textarea>
+                        
+                        @error('body')
+                            <div class="mt-2 text-red-500 text-sm">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div>
+                        <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium">Post</button>
+                    </div>
 
-            </form>
-
+                </form>
+            @endauth
+            
             @if ($posts->count())
                 @foreach ($posts as $post)
-                    <div class="mb-4">
-                        <a href="{{ route('users.posts', $post->user) }}" class="font-bold">{{ $post->user->name }}</a> <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
-                        <p class="mb-2">{{ $post->body }}</p>
-                        @can('delete', $post)
-                            <form action="{{ route('posts.destroy', $post) }}" method="POST" class="mr-1">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="text-red-500">Delete</button>
-                            </form>
-                        @endcan
-                            
-                        <div class="flex items-center">
-                            @auth
-                                @if (!$post->likedBy(auth()->user()))
-                                    <form action="{{ route('posts.likes', $post) }}" method="POST" class="mr-1">
-                                        @csrf
-                                        <button type="submit" class="text-blue-500">Like</button>
-                                    </form>
-                                @else
-                                    <form action="{{ route('posts.likes', $post) }}" method="POST" class="mr-1">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="text-blue-500">Dislike</button>
-                                    </form>
-                                @endif
-                            @endauth
-
-                            <span>
-                                {{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}
-                            </span>
-                        </div>
-                    </div>
+                    <x-post :post="$post" />
                 @endforeach
                 {{ $posts->links() }}
             @else
